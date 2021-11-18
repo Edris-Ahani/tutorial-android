70462cd6dce4f4af57bc9b07034094d8991490fa
edris <edris.ahani.tutorial@gmail.com>
2021-11-18 12:10:12 +0330

    to add custom notification and another notification

---
+++ app/src/main/java/ir/javatime/notification/Example1/AlarmReceiver.java
@@ -0,0 +1,12 @@
+package ir.javatime.notification.Example1;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
--- app/src/main/java/ir/javatime/notification/Example1/MainActivity.java
+++ app/src/main/java/ir/javatime/notification/Example1/MainActivity.java
@@ -1,33 +1,48 @@
 package ir.javatime.notification.Example1;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RemoteViews;
+import android.widget.Toast;
 
 import ir.javatime.notification.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static String TAG = MainActivity.class.getName();
+
+    static String CHANNEL_ID = "notif1";
     Button btn_simple_notif, btn_big_picture_style, btn_big_text_style,
-            btn_inbox_style;
+            btn_inbox_style, btn_custom_layout1, btn_notif1, btn_notif2
+            ,btn_notif3;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        context = this;
         setContentView(R.layout.activity_main);
 
         btn_simple_notif = findViewById(R.id.btn_simple_notif);
         btn_big_picture_style = findViewById(R.id.btn_big_picture_style);
         btn_big_text_style = findViewById(R.id.btn_big_text_style);
         btn_inbox_style = findViewById(R.id.btn_inbox_style);
+        btn_custom_layout1 = findViewById(R.id.btn_custom_layout1);
+        btn_notif1 = findViewById(R.id.btn_notif1);
+        btn_notif2 = findViewById(R.id.btn_notif2);
+        btn_notif3 = findViewById(R.id.btn_notif3);
 
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
@@ -113,12 +128,110 @@ public class MainActivity extends AppCompatActivity {
                 notificationManager.notify(1789, inboxStyle);
             }
         });
+        btn_custom_layout1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get the layouts to use in the custom notification
+                RemoteViews notificationLayout = new RemoteViews(getPackageName(), R.layout.notification_small);
+                //RemoteViews notificationLayoutExpanded = new RemoteViews(getPackageName(), R.layout.notification_large);
+
+                // Apply the layouts to the notification
+                Notification customNotification = new NotificationCompat.Builder(MainActivity.this, "notif1")
+                        .setSmallIcon(android.R.drawable.stat_notify_chat)
+                        .setContentTitle("Title Notif1")
+                        .setContentText("this is content text")
+                        .setStyle(new NotificationCompat.DecoratedCustomViewStyle())
+                        .setCustomContentView(notificationLayout)
+                        //.setCustomBigContentView(notificationLayoutExpanded)
+                        .build();
+                notificationManager.notify(1908, customNotification);
+            }
+        });
+
+        btn_notif1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Notification notification = new NotificationCompat.Builder(context, CHANNEL_ID)
+                        .setSmallIcon(R.drawable.user)
+                        .setContentTitle("imageTitle")
+                        .setContentText("imageDescription")
+                        .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.user))
+                        .setStyle(new NotificationCompat.BigPictureStyle()
+                                .bigPicture(BitmapFactory.decodeResource(getResources(),
+                                        R.drawable.user))
+                                .bigLargeIcon(null))
+                        .build();
+                notificationManager.notify(1908, notification);
+            }
+        });
+
+        btn_notif2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Notification notification = new NotificationCompat.Builder(context, CHANNEL_ID)
+                        .setSmallIcon(android.R.drawable.stat_notify_chat)
+                        .setContentTitle("title")
+                        .setContentText("text")
+                        .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.user))
+                        .setStyle(new NotificationCompat.BigTextStyle()
+                                .bigText("this is big text.this is big text.this is big text." +
+                                        "this is big text.this is big text.this is big text." +
+                                        "this is big text.this is big text.this is big text." +
+                                        "this is big text.this is big text.this is big text."))
+                        .build();
+                notificationManager.notify(1908, notification);
+            }
+        });
+
+        btn_notif3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,
+                        MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|
+                        Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                intent.putExtra("MY_TAG", "result");
+                PendingIntent pendingIntent1 =
+                        PendingIntent.getActivity(MainActivity.this,
+                                9874, intent, 0);
+                PendingIntent pendingIntent2 =
+                        PendingIntent.getActivity(MainActivity.this,
+                                9888, intent, 0);
+                PendingIntent pendingIntent3 =
+                        PendingIntent.getActivity(MainActivity.this,
+                                10988, intent, 0);
+
+                Notification notification = new NotificationCompat.Builder(context, CHANNEL_ID)
+                        // Show controls on lock screen even when user hides sensitive content.
+                        .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+                        .setSmallIcon(R.drawable.user)
+                        // Add media control buttons that invoke intents in your media service
+                        .addAction(R.drawable.ic_previous, "Previous", pendingIntent1) // #0
+                        .addAction(R.drawable.ic_pause, "Pause", pendingIntent2)  // #1
+                        .addAction(R.drawable.ic_next, "Next", pendingIntent3)     // #2
+                        // Apply the media style template
+                        /*.setStyle(new android.support.v4.media.app.Notification.MediaStyle()
+                                .setShowActionsInCompactView(1 )
+                                .setMediaSession(mediaSession.getSessionToken()))*/
+                        .setContentTitle("Wonderful music")
+                        .setContentText("My Awesome Band")
+                        .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.user))
+                        .build();
+                notificationManager.notify(1908, notification);
+            }
+        });
 
 
+
+
+        Intent intent = getIntent();
+        Bundle extras = getIntent().getExtras();
+        String myTagValue = getIntent().getStringExtra("MY_TAG");
+        Log.i(TAG, "onCreate: ");
     }
+    
+    /*public void notifBtn(View view){
+        Toast.makeText(context, "test", Toast.LENGTH_SHORT).show();
+    }*/
 
-
-    public void bigPictureStyle(){
-
-    }
 }
